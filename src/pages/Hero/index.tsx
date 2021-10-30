1c29ab5e1d21cc631b28e3779b7031f629cbde73
Rafael Makaha <rafael.makaha@gmail.com>
2021-10-30 12:15:45 -0300

    Adds hero description card on hero page
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- src/pages/Hero/index.tsx
+++ src/pages/Hero/index.tsx
@@ -5,14 +5,14 @@ import { Page } from '../../components/Page'
 import { Header } from '../../components/Header'
 import { useAppSelector } from '../../store'
 
-import { Content } from './styles'
+import { Content, InfoCard, Text } from './styles'
 
 export const Hero = () => {
   const history = useHistory()
   const heroPage = useAppSelector((state) => state.heroesReducer.selectedHero)
 
   useEffect(() => {
-    if(!heroPage) history.push('/')
+    if (!heroPage) history.push('/')
   }, [history, heroPage])
 
   return (
@@ -21,7 +21,27 @@ export const Hero = () => {
         {heroPage?.localized_name}
       </Header>
       <Content>
-        <img src={`https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/${heroPage?.img}.png`} alt={heroPage?.name}/>
+        <InfoCard>
+          <Text>
+            Attack Type: <strong>{heroPage?.attack_type}</strong>
+          </Text>
+          <Text>
+            Primary Attribute: <strong>{heroPage?.primary_attr.toUpperCase()}</strong>{'\n'}
+          </Text>
+          <Text>
+            Roles:
+            <ul>
+              {heroPage?.roles.map(role => (
+                <Text>
+                  <li>
+                    {role}
+                  </li>
+                </Text>
+              ))}
+            </ul>
+          </Text>
+        </InfoCard>
+        <img src={`https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/${heroPage?.img}.png`} alt={heroPage?.name} />
       </Content>
     </Page>
   )
--- src/pages/Hero/styles.ts
+++ src/pages/Hero/styles.ts
@@ -1,5 +1,29 @@
 import styled from 'styled-components'
 
 export const Content = styled.div`
+    
+`
 
+export const InfoCard = styled.div`
+    align-items: flex-start;
+    background-color: ${(props) => props.theme.cardBackground};
+    border-radius: 4px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    padding: 16px 16px 0 16px;
+    position: absolute;
+    width: 40%;
+`
+
+export const Text = styled.p`
+    color: ${(props) => props.theme.colors.black};
+
+    strong {
+        color: ${(props) => props.theme.colors.primaryRed};
+    }
+
+    li {
+        margin: 20px;
+    }
 `
